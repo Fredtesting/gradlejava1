aaaf5876d578182b180f572d3f3508799ba8bed9
Voronchikhin Fedor <fredsergeevich@outlook.com>
2022-12-18 22:30:06 +0300

    Third commit

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -1,26 +1,26 @@
 package ru.netology.service;
 
-import org.junit.Test;
+import org.testng.annotations.Test;
 
-import static org.junit.Assert.*;
+import static org.testng.Assert.*;
 
 public class CashbackHackServiceTest {
 
     @Test
-    public void remain() {
+    public void testRemain() {
         CashbackHackService cashbackHackService = new CashbackHackService();
-        int amount=1800;
+        int amount = 900;
         int actual = cashbackHackService.remain(amount);
-        int expected = 200;
-        assertEquals(expected, actual);
+        int expected = 100;
+        assertEquals(actual,expected);
     }
     @Test
-    public void shouldNotRemain() {
+    public void testNotRemain() {
         CashbackHackService cashbackHackService = new CashbackHackService();
-        int amount=2000;
+        int amount = 2000;
         int actual = cashbackHackService.remain(amount);
         int expected = 0;
-        assertEquals(expected, actual);
+        assertEquals(actual,expected);
     }
 
 }
